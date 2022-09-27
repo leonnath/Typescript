9e0a9ffc0b16f70b13cd33ab934f04a931b02728
nath <nathgarcia2903@gmail.com>
2022-09-27 23:43:30 +0200

    add the new files

---
+++ tipos/arrays.ts
@@ -0,0 +1,10 @@
+(() => {
+   // const number: (string | number | boolean )[] = [1, 2, 3, 4, 5, '6', 7, 8, 9, 10]
+    const number: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    
+    const villians = ['Omega Rojo', 'Dormammu', 'Duende Verde']
+
+    villians.forEach( v => console.log(v.toUpperCase() ) )
+
+
+})()
---
+++ tipos/tuples.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    const hero = ['Dr Strange', 100];
+    hero[0] = 'Ironman';
+    hero[1] = 50;
+    hero[2] = false;
+    console.log(hero);
+})();
